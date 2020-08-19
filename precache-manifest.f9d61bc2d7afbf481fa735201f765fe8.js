faab0e9adeeb9b542e73fadd7520ba0c86789815
unknown <jakob.carlbring@gmail.com>
2020-08-19 14:21:23 +0200

    Updates

---
+++ precache-manifest.f9d61bc2d7afbf481fa735201f765fe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eff1b0886576f99fcd6a1ca945e6cc5",
+    "url": "/cats/index.html"
+  },
+  {
+    "revision": "2da8230d5a3703f8bf9f",
+    "url": "/cats/static/css/main.c9815a96.chunk.css"
+  },
+  {
+    "revision": "518bae9ab717a817b47d",
+    "url": "/cats/static/js/2.7de35bd4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cats/static/js/2.7de35bd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da8230d5a3703f8bf9f",
+    "url": "/cats/static/js/main.6a2a3323.chunk.js"
+  },
+  {
+    "revision": "344318946af31a6c994f",
+    "url": "/cats/static/js/runtime-main.83494932.js"
+  }
+]);
